98642f4a859331a3730c96e103886dad353e10f5
K2122348H <99530237+K2122348H@users.noreply.github.com>
2022-02-12 17:01:05 +0800

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def index():
         rates = request.form.get("rates")
         print(rates)
         model = joblib.load("DBS")
-        pred = model.predict([[rates]])
+        pred = model.predict([[float(rates)]])
         print(pred)
         s = "The predicted DBS share price is " + str(pred[0][0])
         return (render_template("index.html", result=s))
